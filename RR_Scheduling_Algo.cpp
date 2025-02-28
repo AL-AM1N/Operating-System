a77e7d99963f2242070553f4ac8acdca31f3a266
MD. AL-AMIN <152994093+AL-AM1N@users.noreply.github.com>
2025-02-28 22:37:18 +0600

    Round Robin Scheduling Algo added

---
+++ RR_Scheduling_Algo.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+struct Process {
+  int id;
+  int arrivalTime;
+  int burstTime;
+  int completionTime;
+  int turnaroundTime;
+  int waitingTime;
+  int responseTime;
+};
+
+void calculateTimes(Process processes[], int n, int quantum) {
+  int remainingTime[n];
+  for (int i = 0; i < n; i++) {
+    remainingTime[i] = processes[i].burstTime;
+    processes[i].responseTime = -1;
+  }
+  int currentTime = 0;
+  bool allDone = false;
+  while (!allDone) {
+    allDone = true;
+    for (int i = 0; i < n; i++) {
+      if (remainingTime[i] > 0) {
+        allDone = false;
+        if (processes[i].responseTime == -1) {
+          processes[i].responseTime = currentTime - processes[i].arrivalTime;
+        }
+        if (remainingTime[i] > quantum) {
+          currentTime = currentTime + quantum;
+          remainingTime[i] = remainingTime[i] - quantum;
+        } else {
+          currentTime = currentTime + remainingTime[i];
+          processes[i].completionTime = currentTime;
+          remainingTime[i] = 0;
+        }
+      }
+    }
+  }
+}
+
+void calculateTurnaroundTime(Process processes[], int n) {
+  for (int i = 0; i < n; i++)
+    processes[i].turnaroundTime =
+        processes[i].completionTime - processes[i].arrivalTime;
+}
+
+void calculateWaitingTime(Process processes[], int n) {
+  for (int i = 0; i < n; i++)
+    processes[i].waitingTime =
+        processes[i].turnaroundTime - processes[i].burstTime;
+}
+
+void printTable(Process processes[], int n) {
+  cout << "--------------------------------------------------------------------"
+          "----------------------\n";
+  cout << "| Process | Arrival Time | Burst Time | Completion Time | "
+          "Turnaround Time | Waiting Time | Response Time |\n";
+  cout << "--------------------------------------------------------------------"
+          "----------------------\n";
+  for (int i = 0; i < n; i++) {
+    cout << "|    " << processes[i].id << "    |      "
+         << processes[i].arrivalTime << "      |     " << processes[i].burstTime
+         << "     |        " << processes[i].completionTime
+         << "        |        " << processes[i].turnaroundTime
+         << "         |      " << processes[i].waitingTime
+         << "      |      " << processes[i].responseTime << "      |\n";
+  }
+  cout << "--------------------------------------------------------------------"
+          "----------------------\n";
+}
+
+int main() {
+  int n, quantum;
+  cout << "Enter The Number of Process";
+  cin >> n;
+  cout << "Enter The Time Quantum";
+  cin >> quantum;
+
+  Process processes[n];
+  cout << "Enter process details:\n";
+  for (int i = 0; i < n; i++) {
+    cout << "Process " << i + 1 << ":\n";
+    processes[i].id = i + 1;
+    cout << "   Arrival Time: ";
+    cin >> processes[i].arrivalTime;
+    cout << "   Burst Time: ";
+    cin >> processes[i].burstTime;
+  }
+
+  calculateTimes(processes, n, quantum);
+  calculateTurnaroundTime(processes, n);
+  calculateWaitingTime(processes, n);
+
+  cout << "\nRound Robin Scheduling Results:\n";
+  printTable(processes, n);
+
+  return 0;
+}
+
